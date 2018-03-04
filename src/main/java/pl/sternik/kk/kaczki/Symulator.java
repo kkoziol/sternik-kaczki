4ae15c0bea44d186fe0d3bbf0140c0441291f429
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-04 10:09:21 +0100

    Symulator

---
+++ src/main/java/pl/sternik/kk/kaczki/Symulator.java
@@ -0,0 +1,43 @@
+package pl.sternik.kk.kaczki;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.sternik.kk.kaczki.ges.Ges;
+import pl.sternik.kk.kaczki.ges.GesKaczkaAdapter;
+import pl.sternik.kk.kaczki.kwakanie.Niemowa;
+
+public class Symulator {
+
+	public static void main(String[] args) {
+
+		Kaczka dzika = new DzikaKaczka("dziwaczka");
+		
+		dzika.kwacz();
+		dzika.lataj();
+		dzika.wyswietl();
+		
+		Kaczka gumowa = new GumowaKaczka("żółta");
+		Kaczka drewniana = new DrewnianaKaczka("Dębowa");
+		Kaczka mandarynka = new MandarynkaKaczka("samiec");
+
+		List<Kaczka> kaczki = new ArrayList<Kaczka>();
+		kaczki.add(dzika);
+		kaczki.add(gumowa);
+		kaczki.add(drewniana);
+		kaczki.add(mandarynka);
+		
+		for (Kaczka kaczka : kaczki) {
+			kaczka.wyswietl();
+			kaczka.lataj();
+			kaczka.plywaj();
+			kaczka.kwacz();
+		}
+		System.out.println("-------------------------------");
+		gumowa.kwacz();
+		
+		gumowa.setKwakanie(new Niemowa());
+		
+		gumowa.kwacz();
+	}
+}
